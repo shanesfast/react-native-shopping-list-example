11f5912a37eed654747f4ba2d2cc301f6d0bb9ea
Shane Quick <shanesfast@gmail.com>
2020-05-21 15:50:22 -0500

    Build out basic functionaily fo shopping list

--- App.js
+++ App.js
@@ -1,29 +1,50 @@
-import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Alert, FlatList, StyleSheet, View } from 'react-native'
+import uuid from 'react-native-uuid'
+import AddItem from './components/AddItem'
+import Header from './components/Header'
+import ListItem from './components/ListItem'
 
 const styles = StyleSheet.create({
   conatiner: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }, 
-  mainText: {
-    fontSize: 30,
-    color: 'darkslateblue'
-  }, 
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2
+    paddingTop: 60
   }
 })
 
 export default function App() {
+  const [items, setItems] = useState([
+    { id: uuid.v4(), text: 'Granola' },
+    { id: uuid.v4(), text: 'Bread' },
+    { id: uuid.v4(), text: 'Cheese' },
+    { id: uuid.v4(), text: 'Almond milk' },
+    { id: uuid.v4(), text: 'Bananas' },
+    { id: uuid.v4(), text: 'Apples' },
+  ])
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id)
+    })
+  }
+
+  const addItem = (item) => {
+    if (!item) {
+      Alert.alert('Error', 'Item is blank. Please enter an item.', { text: 'Ok!' })
+    } else {
+      setItems(prevItems => {
+        return [...prevItems, {id: uuid.v4(), text: item}]
+      })
+    }
+  }
+
   return (
     <View style={styles.conatiner}>
-      <Text style={styles.mainText}>Hello world!!</Text>
-      <Image source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
-             style={styles.img} />
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList 
+        data={items} 
+        renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem}/>} />
     </View>
   )
 }
